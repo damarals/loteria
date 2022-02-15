352d30b64da6c297dfea2c647b6b41d0560fcea7
Daniel Amaral <silva.daniel86@Gmail.com>
2022-02-15 02:31:17 -0300

    :bug: add nomes com hifen para correta util da api

--- data-raw/atualizar_sorteios.R
+++ data-raw/atualizar_sorteios.R
@@ -1,15 +1,17 @@
 library(magrittr, include.only = "%>%")
 
-modalidades <- c('megasena', 'lotofacil', 'quina', 'lotomania',
-                 'duplasena', 'diadesorte', 'supersete')
+modalidades <- list(megasena = 'mega-sena', lotofacil = 'lotofacil', quina = 'quina',
+                    lotomania = 'lotomania', duplasena = 'dupla-sena',
+                    diadesorte = 'dia-de-sorte', supersete = 'super-sete')
 
-purrr::walk(modalidades, function(modalidade) {
+purrr::walk(names(modalidades), function(modalidade) {
   if(loteria::necessario_atualizar(modalidade)) {
     max_concurso_offline <- loteria::dados_sorteios(modalidade) %>%
       dplyr::pull(concurso) %>% max
     modalidade_atualizada <- loteria::dados_sorteios(modalidade) %>%
       dplyr::bind_rows(
-        resultado_loteria_todos(modalidade, min_concurso = max_concurso_offline)
+        resultado_loteria_todos(modalidades[[modalidade]],
+                                min_concurso = max_concurso_offline)
       ) %>% dplyr::distinct()
     assign(modalidade, modalidade_atualizada, envir = .GlobalEnv)
     readr::write_csv(modalidade_atualizada, glue::glue('inst/extdata/{modalidade}.csv'))
